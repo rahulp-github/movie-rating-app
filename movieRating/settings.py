4ac5c80a88b829338e24ca58ee2014e42875cb48
rahulp-github <71189359+rahulp-github@users.noreply.github.com>
2021-05-13 11:50:22 +0530

    Update settings.py

--- movieRating/settings.py
+++ movieRating/settings.py
@@ -20,8 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_*fic$4ubx9lba$rgd(qv5lnx*4o1x0nl+)0j17&hodh=+u$&l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
